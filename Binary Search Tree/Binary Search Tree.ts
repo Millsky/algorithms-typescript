e4d6c2dda32a4f54eda2f66083e8a0c9f1c32099
millsky <kyle83567@gmail.com>
2017-03-23 22:21:31 -0400

    Updated to add an initial test - this will be cleaned up later.
    1.) Function to rotate tree right
    2.) Function to rotate tree left
    3.) Function to update heights of tree

--- Binary Search Tree/Binary Search Tree.ts
+++ Binary Search Tree/Binary Search Tree.ts
@@ -3,13 +3,15 @@ class node {
     value: any
     left: any
     right: any
+    /* Store height to reduce run time */
     height: number
     /* Function to compute the balance factor which we will use later */
-    balanceFactor(){
+    balanceFactor() {
         var bf = (this.right.height) - (this.left.height);
 
     }
-    constructor(v,h) {
+
+    constructor(v, h) {
         this.value = v;
         this.height = h;
     }
@@ -17,6 +19,86 @@ class node {
 
 /* BST - Main class */
 class bst {
+
+
+    rotationTest() {
+        /* TEST VALUES FOR ROTATION */
+        var b = new bst();
+        b.addValue(10);
+        b.addValue(3);
+        b.addValue(11);
+        b.addValue(12);
+        b.addValue(15);
+        return b.rotateLeft(b.root);
+    }
+
+    updateLinkedNodeHeights(treeNode: node, delta: number) {
+        var currentNode = treeNode;
+        console.log("Updating height of:");
+        console.log(treeNode);
+
+        currentNode.height = currentNode.height + delta;
+
+        /* Set current Node update both right and left heights */
+        /* if left set current node to left repeat */
+        /* if right set current node to right repeat */
+
+
+        if (currentNode.left) {
+            console.log("Recursing Left");
+            //currentNode.left.height = currentNode.left.height + delta;
+            let tempNode = currentNode;
+            currentNode.left = this.updateLinkedNodeHeights(tempNode.left, delta);
+        }
+        if (currentNode.right) {
+            console.log("Recursing Right");
+            //currentNode.right.height = currentNode.right.height + delta;
+            let tempNode = currentNode;
+            currentNode.right = this.updateLinkedNodeHeights(tempNode.right, delta);
+        }
+
+
+        return currentNode;
+    }
+
+    /* Rotation is basically copying existing nodes and inserting them */
+    rotateLeft(treeNode: node) {
+
+        let Pivot = treeNode.right;
+
+        treeNode.right = Pivot.left;
+
+        Pivot.left = treeNode;
+
+        Pivot.left = this.updateLinkedNodeHeights(Pivot.left, 1);
+
+        treeNode = Pivot;
+
+        treeNode.right = this.updateLinkedNodeHeights(treeNode.right, -1);
+
+        treeNode.height = treeNode.height - 1;
+
+        return treeNode;
+    }
+
+    rotateRight(treeNode: node) {
+        let Pivot = treeNode.left;
+
+        treeNode.left = Pivot.right;
+
+        Pivot.right = treeNode;
+
+        Pivot.right = this.updateLinkedNodeHeights(Pivot.right, 1);
+
+        treeNode = Pivot;
+
+        treeNode.left = this.updateLinkedNodeHeights(treeNode.left, -1);
+
+        treeNode.height = treeNode.height - 1;
+
+        return treeNode;
+    }
+
     root: node
 
     addValue(v) {
@@ -24,7 +106,7 @@ class bst {
         /* IF ROOT NOT INIT ADD IT */
 
         if (!this.root) {
-            this.root = new node(v,0);
+            this.root = new node(v, 0);
             return;
         }
 
@@ -37,7 +119,7 @@ class bst {
             /* If less go left */
             if (v < currentNode.value) {
                 if (!currentNode.left) {
-                    currentNode.left =  new node(v,height);
+                    currentNode.left = new node(v, height);
                     break;
                 } else {
                     currentNode = currentNode.left;
@@ -45,17 +127,19 @@ class bst {
             } else {
                 /* If more go right */
                 if (!currentNode.right) {
-                    currentNode.right = new node(v,height);
+                    currentNode.right = new node(v, height);
                     break;
                 } else {
                     currentNode = currentNode.right;
                 }
             }
             /* If node has value we want to continue to check it's children so we
-            recursively perform operation on the left or rightmost node*/
+             recursively perform operation on the left or rightmost node*/
         }
 
     }
 
 }
 
+var m = new bst();
+m.root = m.rotationTest();
